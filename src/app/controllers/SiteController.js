e0f2f1022f109e3c29f9e636ee010f906f6970d5
phudnd <38520225+dndphu@users.noreply.github.com>
2023-09-10 14:34:46 +0700

    add controler get model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,23 @@
+const Course = require("../models/Course");
+
+async function getCourse() {
+  const course = await Course.find({});
+  return course;
+}
 class NewsController {
   //[GET] /
   index(req, res) {
-    res.render("home");
+    // await Course.find({}, function (err, courses) {
+    //   if (!err) res.json(courses);
+    //   res.status(400).json({ error: err });
+    // });
+    getCourse()
+      .then((course) => {
+            res.json(course);
+      })
+      .catch((err) => {
+        res.status(400).json({ error: err });
+      });
   }
 
   //[GET] /search
