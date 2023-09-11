a0497a4a54c827fab8c780c9fcc59f6d2955e21d
phudnd <38520225+dndphu@users.noreply.github.com>
2023-09-11 23:37:48 +0700

    fix typo

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,7 +5,6 @@ const courseController = require("../app/controllers/CourseController");
 
 router.get("/courses/create", courseController.create);
 router.get("/courses/:slug", courseController.show);
-
 router.get("/courses/:id/edit", courseController.edit);
 
 router.post("/courses/store", courseController.store);
